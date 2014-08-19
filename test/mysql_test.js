b906f5016c4428e18c192bbaade3b32465a48b1e
Ben Hood <0x6e6562@gmail.com>
2014-08-19 13:31:54 +0100

    Move the data insert into the callback after the DDL migration

--- test/mysql_test.js
+++ test/mysql_test.js
@@ -28,30 +28,30 @@ before(function(done){
 
   postgrator.migrate('1', function (err, migrations) {
     if (err) done(err);
+
+    client.connect();
+
+    client.query("TRUNCATE TABLE call_records;", function(err, res) {
+      if (err) done(err);
+    });
+
+    var calls = _.range(10).map(function (n) { 
+      var imsi = 230023741299234 + n + '';
+      var duration = chance.integer({min: 1, max: 100});
+      var timestamp = chance.date({year: 2014});
+      var caller = chance.phone();
+      var callee = chance.phone();
+      return [imsi, timestamp, duration, caller, callee];
+    });
+
+    var sql = "INSERT INTO call_records (imsi, timestamp, duration, calling_number, called_number) VALUES ?";
+
+    client.query(sql, [calls], function(err) {
+        done(err);
+    });
+
+    client.end();
   });
-
-  client.connect();
-
-  client.query("TRUNCATE TABLE call_records;", function(err, res) {
-  	if (err) done(err);
-  });
-
-  var calls = _.range(10).map(function (n) { 
-    var imsi = 230023741299234 + n + '';
-    var duration = chance.integer({min: 1, max: 100});
-    var timestamp = chance.date({year: 2014});
-    var caller = chance.phone();
-    var callee = chance.phone();
-    return [imsi, timestamp, duration, caller, callee];
-  });
-
-  var sql = "INSERT INTO call_records (imsi, timestamp, duration, calling_number, called_number) VALUES ?";
-
-  client.query(sql, [calls], function(err) {
-      done(err);
-  });
-
-  client.end();
 
 })
 
